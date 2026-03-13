4dc259c1c8abdbfce2e6e59df6042270e8efc272
Wilian <wiliang599@gmail.com>
2026-03-13 15:35:40 -0300

    Aula sobre Dados não primitivos e métodos

--- src/TiposDeDados/DadosNaoPrimitivos.java
+++ src/TiposDeDados/DadosNaoPrimitivos.java
@@ -2,8 +2,22 @@ package TiposDeDados;
 
 public class DadosNaoPrimitivos {
     public static void main(String[] args) {
+        // Tudo que for digitado aqui dentro com o comando PSVM, será compilado pelo Java
 
-        
+        // Dados não primitivos: String, Array, Class, enum
+        // Objetivo da aula: criar um ninja e atribuir métodos a ele.
+
+        String nome = "Naruto Uzumaki";
+        System.out.println("Nome do ninja: " + nome);
+
+        String nomeUpperCase = nome.toUpperCase();  // Método uppercase - Tudo em MAIÚSCULO
+        System.out.println("Nome do ninja em MAIÚSCULO: " + nomeUpperCase);
+
+        String aldeia = "ALDEIA DA FOLHA";
+        System.out.println("\nNome da aldeia: " + aldeia);
+
+        String aldeiaLowerCase = aldeia.toLowerCase();  // Método lowercase - Tudo em MINÚSCULO
+        System.out.println("Nome da aldeia MINÚSCULO: " + aldeiaLowerCase);
 
     }
 }
--- src/TiposDeDados/DadosPrimitivos.java
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -2,17 +2,17 @@ package TiposDeDados;
 
 public class DadosPrimitivos {
     public static void main(String[] args) {
+        // Tudo que for digitado aqui dentro com o comando PSVM, será compilado pelo Java
 
         // Dados Primitivos em Java
+        // Objetivo da aula: criar um ninja
 
-        // Valor máximo do tipo int: 2147483647
-        int idade = 16;
+        int idade = 16;     // Valor máximo do tipo int: 2147483647
         double altura = 1.65;
         char inicial = 'N';
         boolean vivoOuMorto = true;
         // O tipo long no valor deve ter um L no final
-        // Valor máximo do Long: 9223372036854775807
-        long saldoBancario = 2147483648L;
+        long saldoBancario = 2147483648L;   // Valor máximo do Long: 9223372036854775807
 
         // Comando para mostrar para o usuário
         System.out.println("Idade: " + idade);
